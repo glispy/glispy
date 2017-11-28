80669d58c35ee2e9e44370dfec7c2e4f58e4892a
itsMontoya <josh@montoya.io>
2017-11-28 09:07:55 -0800

    Expand benchmarking

--- glisp_test.go
+++ glisp_test.go
@@ -4,28 +4,109 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/missionMeteora/journaler"
+	"github.com/janne/go-lisp/lisp"
 )
 
 const (
 	program = "(begin (define r 10) (* pi (* r r)))"
 )
 
+var (
+	glispSink  Expression
+	goLispSink lisp.Value
+)
+
 func TestGlisp(t *testing.T) {
 	g := NewGlisp()
 	tkns := NewTokens(`(+ 1 3 (+ 2 5))`)
-	journaler.Debug("Tokens: %v", tkns)
 
 	exp, err := NewExpression(&tkns)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	journaler.Debug("Expression: %v", exp)
 	out, err := g.Eval(exp)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	fmt.Println(out)
+}
+
+func BenchmarkGlispAdd(b *testing.B) {
+	var (
+		exp Expression
+		val Expression
+		err error
+	)
+
+	g := NewGlisp()
+
+	for i := 0; i < b.N; i++ {
+		tkns := NewTokens(`(+ 1 3 (+ 2 5))`)
+		if exp, err = NewExpression(&tkns); err != nil {
+			b.Fatal(err)
+		}
+
+		if val, err = g.Eval(exp); err != nil {
+			b.Fatal(err)
+		}
+
+		if val.(Number) != 11 {
+			b.Fatalf("invalid value, expected %v and received %v", 11, val)
+		}
+
+		glispSink = val
+	}
+
+	b.ReportAllocs()
+}
+
+func BenchmarkGlispAdd_PreProcessed(b *testing.B) {
+	var (
+		exp Expression
+		val Expression
+		err error
+	)
+
+	g := NewGlisp()
+	tkns := NewTokens(`(+ 1 3 (+ 2 5))`)
+
+	if exp, err = NewExpression(&tkns); err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		if val, err = g.Eval(exp); err != nil {
+			b.Fatal(err)
+		}
+
+		if val.(Number) != 11 {
+			b.Fatalf("invalid value, expected %v and received %v", 11, val)
+		}
+
+		glispSink = val
+	}
+
+	b.ReportAllocs()
+}
+
+func BenchmarkGoLispAdd(b *testing.B) {
+	var (
+		val lisp.Value
+		err error
+	)
+	for i := 0; i < b.N; i++ {
+		if val, err = lisp.EvalString(`(+ 1 3 (+ 2 5))`); err != nil {
+			b.Fatal(err)
+		}
+
+		if val.Number() != 11 {
+			b.Fatalf("invalid value, expected %v and received %v", 11, val)
+		}
+
+		goLispSink = val
+	}
+
+	b.ReportAllocs()
 }
