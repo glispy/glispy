e70c936dd8fecb021e543f3b84152482d5075856
Josh Montoya <josh@montoya.io>
2020-04-30 09:06:55 -0700

    Implement scope type

---
+++ types/scope.go
@@ -0,0 +1,8 @@
+package types
+
+// Scope represents a scope type
+type Scope interface {
+	Get(key Symbol) (out Expression, ok bool)
+	Put(key Symbol, in Expression)
+	PutRoot(key Symbol, in Expression)
+}
