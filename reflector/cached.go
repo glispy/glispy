1b6c7da2939eb6f26352d7d309a89f453eaa1712
Josh Montoya <josh@montoya.io>
2020-05-05 07:41:16 -0700

    Add type caching

--- reflector/cached.go
+++ reflector/cached.go
@@ -11,11 +11,24 @@ func makeCached(t reflect.Type, tag string) (c Cached) {
 			continue
 		}
 
-		c[alias] = i
+		c[alias] = newCachedEntry(i, field.Type)
 	}
 
 	return
 }
 
 // Cached manages the reflection fields by key for a given type
-type Cached map[string]int
+type Cached map[string]*CachedEntry
+
+func newCachedEntry(index int, rType reflect.Type) *CachedEntry {
+	var e CachedEntry
+	e.Index = index
+	e.Type = rType
+	return &e
+}
+
+// CachedEntry holds the index and type of a field
+type CachedEntry struct {
+	Index int
+	Type  reflect.Type
+}
