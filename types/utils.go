ff152c065bf78270e8d75c3bcc22fe11d1c91131
Josh Montoya <josh@montoya.io>
2020-06-09 10:50:17 -0700

    Implement ToNumber

--- types/utils.go
+++ types/utils.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 // ToAtom will convert a value to the atom representation
 func ToAtom(val interface{}) (a Atom) {
 	switch n := val.(type) {
@@ -10,4 +12,33 @@ func ToAtom(val interface{}) (a Atom) {
 	}
 
 	return Atom(val)
+}
+
+// ToNumber converts a value to a number
+func ToNumber(a Atom) (n Number, err error) {
+	switch val := a.(type) {
+	case Number:
+		n = val
+	case int:
+		n = Number(val)
+	case int32:
+		n = Number(val)
+	case int64:
+		n = Number(val)
+	case uint:
+		n = Number(val)
+	case uint32:
+		n = Number(val)
+	case uint64:
+		n = Number(val)
+	case float32:
+		n = Number(val)
+	case float64:
+		n = Number(val)
+
+	default:
+		err = fmt.Errorf("value of %v cannot be converted to a number", a)
+	}
+
+	return
 }
